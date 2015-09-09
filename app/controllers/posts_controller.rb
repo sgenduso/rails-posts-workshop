333c3098e01b7a580db94fbc45658a46d9b3599e
Sam Genduso <sam.genduso@gmail.com>
2015-09-09 12:03:01 -0600

    first rails app, manual CRUD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # @post = Post.new(params[:post])
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+  end
+end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :author, :body)
+  end
+
+  end
