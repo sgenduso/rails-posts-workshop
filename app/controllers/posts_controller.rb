fa607e85237892f97d0c4fb0540f4b0fa245feea
Sam Genduso <sam.genduso@gmail.com>
2015-09-10 11:33:56 -0600

    show and update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
 
   def new
@@ -14,8 +15,27 @@ class PostsController < ApplicationController
       redirect_to posts_path
     else
       render :new
+    end
   end
-end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    if @post.save
+      redirect_to post_path
+    else
+      render :edit
+    end
+  end
+
 
   private
 
